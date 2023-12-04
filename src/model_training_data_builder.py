a0f9b0bf24268c740203b7ce3b39e99eac9acbba
Chance Neihouse <chance@nei.house>
2023-12-04 14:36:59 -0800

    Implement dataset preparation method in ModelTrainingDataBuilder class

--- src/model_training_data_builder.py
+++ src/model_training_data_builder.py
@@ -1,4 +1,16 @@
+import pandas as pd
+from typing import List
+
+
 class ModelTrainingDataBuilder:
-    def build_training_dataset(self, preprocessed_data):
-        # TODO: Implement the method to prepare datasets for training AI models
-        pass
+    def __init__(self):
+        self.data = pd.DataFrame()
+
+    def add_track_features(self, track_features: dict):
+        self.data = self.data.append(track_features, ignore_index=True)
+
+    def prepare_dataset(self, track_ids: List[str], spotify_integration: 'SpotifyIntegration'):
+        for track_id in track_ids:
+            features = spotify_integration.get_track_features(track_id)
+            self.add_track_features(features)
+        return self.data
